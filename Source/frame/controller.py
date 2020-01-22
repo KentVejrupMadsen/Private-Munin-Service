db4975df4bd0fd9f2624d617a106c4b9128e159d
Kent vejrup Madsen <Kent.vejrup.Madsen@outlook.com>
2020-01-22 11:28:55 +0100

    current iteration

--- Source/frame/controller.py
+++ Source/frame/controller.py
@@ -1,18 +1,23 @@
 from frame.persistence import Persistence
+
 from frame.telemetry import Telemetry
 
 
 class Controller:
     def __init__(self):
-        persistence = Persistence()
+        self.persistence = Persistence()
+        self.telemetry = Telemetry()
 
         pass
 
     def initialise(self):
+
         pass
 
     def execute(self):
+
         pass
 
     def garbage(self):
+
         pass
